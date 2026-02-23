cc7be38a85d8b6e875b368138b728c4a17cfef69
Quirozdev <luisdevv232@gmail.com>
2026-02-22 18:58:06 -0700

    add basename to router

--- src/app/router.tsx
+++ src/app/router.tsx
@@ -2,10 +2,15 @@ import { createBrowserRouter } from "react-router";
 import AppLayout from "@/app/layouts/AppLayout";
 import { countryRoutes } from "@/features/countries/routes";
 
-export const router = createBrowserRouter([
+export const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <AppLayout />,
+      children: countryRoutes,
+    },
+  ],
   {
-    path: "/",
-    element: <AppLayout />,
-    children: countryRoutes,
+    basename: "/country-displayer",
   },
-]);
+);
